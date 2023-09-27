85fe77f0cb048c6247006ea59ee3fb04246f9dd1
Altaf Alam <102013452+Altafalam3@users.noreply.github.com>
2023-09-26 23:55:48 -0700

    Add files via upload

--- IP/src/App.js
+++ IP/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from 'react';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2> callback ref example</h2>
+      <CallbackRefExample />
+
+      <h2>Forwarding ref example</h2>
+      <ForwardRefExample />
     </div>
   );
-}
+};
+
+const ChildComponent = React.forwardRef((props, ref) =>{
+  return <input ref={ref} />;
+})
+
+const ForwardRefExample = () =>{
+  const parentRef = useRef(null);
+  const childRef = useRef(null);
+
+  useEffect(()=>{
+    if(parentRef.current && childRef.current){
+      parentRef.current.focus();
+    }
+  }, [])
+
+  return (
+    <div>
+      <ChildComponent forwardRef={childRef} />
+      <input ref={parentRef} />
+    </div>
+  )
+} 
+
+const CallbackRefExample = () => {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.value = 'Hello Pikachu';
+    }
+  }, []);
+
+  return (
+    <div>
+      <input ref={inputRef} />
+    </div>
+  );
+};
 
 export default App;
